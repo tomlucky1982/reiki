29aeaf908831ecd9c0b09318f5a58062494f8fbf
tomlucky1982 <94172157+tomlucky1982@users.noreply.github.com>
2026-05-01 15:42:31 +0200

    Update analytics.js

--- analytics.js
+++ analytics.js
@@ -6,14 +6,20 @@
     window.dataLayer.push(arguments);
   };
 
+  // najpierw kolejkujemy eventy
+  gtag("js", new Date());
+
+  // ładujemy skrypt
   const script = document.createElement("script");
   script.async = true;
   script.src = "https://www.googletagmanager.com/gtag/js?id=" + GA_ID;
+
+  script.onload = function () {
+    // dopiero po załadowaniu konfigurujemy GA
+    gtag("config", GA_ID, {
+      send_page_view: true
+    });
+  };
+
   document.head.appendChild(script);
-
-  gtag("js", new Date());
-
-  gtag("config", GA_ID, {
-    send_page_view: true
-  });
 })();
